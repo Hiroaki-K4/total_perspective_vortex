83e425e9ee4cff03e9f3306d13729a7c3d295f8b
Hiroaki-K4 <49no19@gmail.com>
2023-08-26 16:00:47 +0900

    Add explore_dataset.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # total_perspective_vortex
+
+```
+python3 srcs/explore_dataset.py --eeg_data_path eeg-motor-movementimagery-dataset-1.0.0/files/S001/S001R01.edf
+```
--- srcs/explore_dataset.py
+++ srcs/explore_dataset.py
@@ -1,9 +1,18 @@
+import mne
+import argparse
 
 
-
-def main():
-    print("vortex")
+def main(eeg_data_path: str):
+    print("eeg_data_path: ", eeg_data_path)
+    raw = mne.io.read_raw_edf(eeg_data_path)
+    print(raw)
+    print(raw.info)
+    print(raw.info["bads"])
+    raw.plot()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--eeg_data_path")
+    args = parser.parse_args()
+    main(args.eeg_data_path)
