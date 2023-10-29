2dba1c17f686af5b37c19157aca41d206ed24c0e
Hiroaki-K4 <49no19@gmail.com>
2023-10-29 17:08:26 +0900

    Add training script

--- test.sh
+++ test.sh
@@ -19,11 +19,16 @@ test_csp() {
     cd ../
 }
 
-test_training() {
+test_exploration() {
     cd srcs
     yes | python3 explore_dataset.py NotShow
     check_result "explore_dataset.py"
     cd ../
+}
+
+test_training() {
+    yes | python3 srcs/train.py --output_model_path model/pipeline.joblib --subjects_num 1
+    check_result "train.py"
 }
 
 python3 -m pip install -r requirements.txt
@@ -31,6 +36,8 @@ python3 -m pip install -r requirements.txt
 if [ $# -eq 1 ]; then
     if [ $1 = "csp" ]; then
         test_csp
+    elif [ $1 = "exploration" ]; then
+        test_exploration
     elif [ $1 = "training" ]; then
         test_training
     else
@@ -40,5 +47,6 @@ if [ $# -eq 1 ]; then
 
 else
     test_csp
+    test_exploration
     test_training
 fi
